8d4219fa9767966dd3af70e3f67228f25f3c1634
RomanBurunkov <romon2002@gmail.com>
2024-11-17 02:09:09 +0300

    Add more Song Class tests

--- tests/song.test.js
+++ tests/song.test.js
@@ -1,7 +1,7 @@
 import Song from '../src/song';
 
 const mockInfo = {
-  time: 60,
+  time: '60',
   date: 'date',
   file: 'song.mp3',
   title: 'song title',
@@ -11,10 +11,37 @@ const mockInfo = {
   lastModified: 'lastModified'
 };
 
-test('Song class exist', () => expect(Song).toBeDefined());
+const mockKVPs = [
+  'OK',
+  'Time: 60',
+  'Date: date',
+  'file: song.mp3',
+  'Title: song title',
+  'Track: song track',
+  'Genre: song genre',
+  'Artist: song artist',
+  'Last-Modified: lastModified',
+];
 
-test('Test flatCopy', () => {
-  const song = new Song(mockInfo);
-  expect(song.flatCopy).toBeDefined();
-  expect(song.flatCopy()).toEqual(mockInfo);
+describe('Tests for Song Class', () => {
+
+  test('Song class exist', () => expect(Song).toBeDefined());
+
+  test('Creating new Song with correct KVP Data should creates a new Song instance', () => {
+    const song = new Song(mockKVPs);
+
+    expect(song.flatCopy()).toEqual(mockInfo);
+  });
+
+  test('Creating new Song with incorrect KVP Data shoud throw an error', () => {
+    expect(() => new Song(['OK', 'QWERTY', ''])).toThrow();
+  });
+
+  test('Test for flatCopy method', () => {
+    const song = new Song(mockInfo);
+    expect(song.flatCopy).toBeDefined();
+    expect(song.flatCopy()).toEqual(mockInfo);
+  });
+
 });
+
